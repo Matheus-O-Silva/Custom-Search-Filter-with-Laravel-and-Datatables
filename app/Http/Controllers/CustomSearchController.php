04fe7a1b751407463bed6ce486e6d751eddbf730
Matheus-O-Silva <matheuso.silva95@gmail.com>
2022-03-27 11:10:54 -0300

    first commit

---
+++ app/Http/Controllers/CustomSearchController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CustomSearchController extends Controller
+{
+    public function index(Request $request)
+    {
+     if(request()->ajax())
+     {
+      if(!empty($request->filter_gender))
+      {
+       $data = DB::table('tbl_customer')
+         ->select('CustomerName', 'Gender', 'Address', 'City', 'PostalCode', 'Country')
+         ->where('Gender', $request->filter_gender)
+         ->where('Country', $request->filter_country)
+         ->get();
+      }
+      else
+      {
+       $data = DB::table('tbl_customer')
+         ->select('CustomerName', 'Gender', 'Address', 'City', 'PostalCode', 'Country')
+         ->get();
+      }
+      return datatables()->of($data)->make(true);
+     }
+     $country_name = DB::table('tbl_customer')
+          ->select('Country')
+          ->groupBy('Country')
+          ->orderBy('Country', 'ASC')
+          ->get();
+     return view('custom_search', compact('country_name'));
+    }
+
+}
---
+++ resources/views/custom_search.blade.php
@@ -0,0 +1,142 @@
+<html>
+ <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DataTables - Individual Column Search in Datatables using Ajax</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+  
+ </head>
+ <body>
+  <div class="container">    
+     <br />
+     <h3 align="center">DataTables - Custom Search in Datatables using Ajax</h3>
+     <br />
+            <br />
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <select name="filter_gender" id="filter_gender" class="form-control" required>
+                            <option value="">Select Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="filter_country" id="filter_country" class="form-control" required>
+                            <option value="">Select Country</option>
+                            @foreach($country_name as $country)
+
+                            <option value="{{ $country->Country }}">{{ $country->Country }}</option>
+
+                            @endforeach
+                        </select>
+                    </div>
+                    
+                    <div class="form-group" align="center">
+                        <button type="button" name="filter" id="filter" class="btn btn-info">Filter</button>
+
+                        <button type="button" name="reset" id="reset" class="btn btn-default">Reset</button>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+            <br />
+   <div class="table-responsive">
+    <table id="customer_data" style="font-size: 13px; width: 100%" class="table table-sm table-hover">
+                    <thead>
+                        <tr>
+                            <th>Customer Name</th>
+                            <th>Gender</th>
+                            <th>Address</th>
+                            <th>City</th>
+                            <th>Postal Code</th>
+                            <th>Country</th>
+                        </tr>
+                    </thead>
+                </table>
+   </div>
+            <br />
+            <br />
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+    fill_datatable();
+
+    function fill_datatable(filter_gender = '', filter_country = '')
+    {
+        var dataTable = $('#customer_data').DataTable({
+            paging: false,
+            processing: true,
+            serverSide: true,
+            scrollY:"350px",
+            scrollCollapse: true,
+            language: {
+                "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Portuguese-Brasil.json"
+            },
+            ajax:{
+                url: "{{ route('customsearch.index') }}",
+                data:{filter_gender:filter_gender, filter_country:filter_country}
+            },
+            columns: [
+                {
+                    data:'CustomerName',
+                    name:'CustomerName'
+                },
+                {
+                    data:'Gender',
+                    name:'Gender'
+                },
+                {
+                    data:'Address',
+                    name:'Address'
+                },
+                {
+                    data:'City',
+                    name:'City'
+                },
+                {
+                    data:'PostalCode',
+                    name:'PostalCode'
+                },
+                {
+                    data:'Country',
+                    name:'Country'
+                }
+            ]
+        });
+    }
+
+    $('#filter').click(function(){
+        var filter_gender = $('#filter_gender').val();
+        var filter_country = $('#filter_country').val();
+
+        if(filter_gender != '' &&  filter_gender != '')
+        {
+            $('#customer_data').DataTable().destroy();
+            fill_datatable(filter_gender, filter_country);
+        }
+        else
+        {
+            alert('Select Both filter option');
+        }
+    });
+
+    $('#reset').click(function(){
+        $('#filter_gender').val('');
+        $('#filter_country').val('');
+        $('#customer_data').DataTable().destroy();
+        fill_datatable();
+    });
+
+});
+</script>
